7af509eb8994f2f63907e0ff0cee76a3594f52ef
Adam Rogers <cldd5678@hotmail.com>
2018-07-23 17:35:39 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Tim',
-    text: "Hey. How's it going?"
-  });
 });
 
 socket.on('disconnect', function() {
